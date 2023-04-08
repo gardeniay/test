319bb2a768d5f3b0c98e4de42356eb6f2205dbd9
DriPho <107779579+gardeniay@users.noreply.github.com>
2023-04-08 23:57:23 +0800

    校赛预选赛

--- A.cpp
+++ A.cpp
@@ -5,15 +5,25 @@ using namespace std;
 typedef long long int ll;
 typedef double dou;
 
-int n;
-int a, b;
+ll ans[5][5];
+ll len[5];
 
-int main() 
+int main()
 {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i < 4; i++)
 	{
-		scanf("%d+%d", &a, &b);
-		printf("%d\n", a + b);
+		scanf("%lld %lld", &ans[i][1], &ans[i][2]);
 	}
+	len[1] = pow(ans[3][1] - ans[1][1], 2) + pow(ans[3][2] - ans[1][2], 2);//AC
+	len[2] = pow(ans[2][1] - ans[1][1], 2) + pow(ans[2][2] - ans[1][2], 2);//AB
+	len[3] = pow(ans[3][1] - ans[2][1], 2) + pow(ans[3][2] - ans[2][2], 2);//CB
+	if (len[2] >= len[1] + len[3])
+	{
+		printf("Longlive Friendship!\n");
+	}
+	else
+	{
+		printf("Rush B!\n");
+	}
+	return 0;
 }
